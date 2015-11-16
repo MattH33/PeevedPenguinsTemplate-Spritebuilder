a4921fdd17507aede2cf5f5f0d84f2594e4f0ef7
Matt H <Matthew@Matts-Air.cg.shawcable.net>
2015-11-16 03:46:51 -0700

    Automatically Committed for MakeGamesWithUs

--- PeevedPenguins.spritebuilder/Source/Gameplay.m
+++ PeevedPenguins.spritebuilder/Source/Gameplay.m
@@ -23,9 +23,6 @@
 // is called when CCB file has completed loading
 - (void)didLoadFromCCB {
     
-    // visualize physics bodies & joints
-    _physicsNode.debugDraw = TRUE;
-    
     // tell this scene to accept touches
     self.userInteractionEnabled = TRUE;
     
