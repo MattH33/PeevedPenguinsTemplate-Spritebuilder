f0f14491a5ff1d6c7ca62401a06e150905f26d24
Matt H <Matthew@Matts-Air.cg.shawcable.net>
2015-11-15 20:49:05 -0700

    Automatically Committed for MakeGamesWithUs

---
+++ PeevedPenguins.spritebuilder/Source/Gameplay.h
@@ -0,0 +1,13 @@
+//
+//  Gameplay.h
+//  PeevedPenguins
+//
+//  Created by Matt H on 2015-11-15.
+//  Copyright © 2015 Apportable. All rights reserved.
+//
+
+#import "CCNode.h"
+
+@interface Gameplay : CCNode
+
+@end
